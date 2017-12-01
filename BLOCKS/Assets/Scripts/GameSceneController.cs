bc47de77eef0349f06cc7e33a162f89c8b0be16a
Ameen Ruhul Mohammad <ameensunny242@gmail.com>
2017-12-01 20:34:20 +0600

    git pushed

---
+++ BLOCKS/Assets/Scripts/GameSceneController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameSceneController : MonoBehaviour {
+
+	public Text gameText;
+
+     private float safeArea =25;
+     private float gamePointer;
+    public GameObject [ ] blockPrefabs;
+    public Player player;
+	public Camera gameCamera; 
+	// Use this for initialization
+	void Start () 
+	{
+		//Instantiate (blockPrefabs[0]);
+		gamePointer= 7;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	 {
+		if(player!=null)
+		{
+		gameCamera.transform.position = new Vector3( player.transform.position.x,
+		                             gameCamera.transform.position.y,gameCamera.transform.position.z);
+
+		   gameText.text= "Score:  " +Mathf.Floor(player.transform.position.x);
+		}
+
+		while(player!= null && gamePointer<player.transform.position.x+safeArea)
+		{
+			         int blockIndex=Random.Range(0,blockPrefabs.Length);
+                       GameObject blockObject= Instantiate(blockPrefabs[blockIndex]);
+					   blockObject.transform.SetParent(this.transform);
+
+             Block block =blockObject.GetComponent<Block>();
+               blockObject.transform.position = new Vector2(gamePointer+block.size/2,0);
+               gamePointer+= block.size;
+	     }
+
+	 }
+}
---
+++ BLOCKS/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public int JumpForce;
+	public int moveVelocity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("space"))
+		 //if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+		
+		{
+			this.GetComponent<Rigidbody2D> ().AddForce(new Vector2(0,JumpForce));
+		}
+        
+		this.GetComponent<Rigidbody2D> ().velocity = new Vector2( moveVelocity,
+		               this.GetComponent<Rigidbody2D> ().velocity.y);
+
+	}
+  
+   void OnTriggerEnter2D(Collider2D c)
+   {
+	   if(c.tag=="Obstacle")
+	   {
+        GameObject.Destroy (this.gameObject);
+	   }
+   }
+
+
+}
